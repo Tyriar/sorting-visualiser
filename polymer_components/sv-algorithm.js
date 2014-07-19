666a541978535440d09610876e6d3cc0e090c003
Tyriar <tyriar@tyriar.com>
2014-07-19 10:52:46 -0700

    Grunt work, start on index

---
+++ polymer_components/sv-algorithm.js
@@ -0,0 +1,9 @@
+/*! sorting-visualiser | (c) 2014 Daniel Imms */
+/*! https://github.com/Tyriar/sorting-visualiser/blob/master/LICENSE */
+(function () {
+  'use strict';
+
+  Polymer('sv-algorithm', {
+
+  });
+})();
