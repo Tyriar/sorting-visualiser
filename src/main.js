6cd1292d2cf6169d3bb374851752f06aa6aefb5a
Daniel Imms <tyriar@tyriar.com>
2016-04-03 02:23:34 -0700

    Remove dead code

--- src/main.js
+++ src/main.js
@@ -9,17 +9,9 @@ var sorts = require('./sort-definitions');
 
 var ARRAY_SIZE = 20;
 
-function init() {
-  //sorts.forEach(runSort);
-  var initialArray = generateRandomArray();
-  sorts.forEach(function (sort) {
-    sort.pane = new SortPane(sort, initialArray);
-  });
+function initEventListeners() {
+  document.querySelector('#play-all').addEventListener('click', playAll);
 }
-
-document.querySelector('#play-all').addEventListener('click', playAll);
-
-init();
 
 function generateRandomArray() {
   var array = [];
@@ -43,96 +35,12 @@ function playAll() {
   });
 }
 
-
-
-function runSort(sortDefinition) {
-  var snap = Snap(sortDefinition.svg);
-
-  // Create dummy data
-  var data = generateRandomArray();
-
-  // Create data rectangles
-  var dataRects = generateElementRectangles(snap, data);
-  var sortActions = sort(data, dataRects, sortDefinition.algorithm);
-  function finishedPlayback() {
-
-  }
-  playSortActions(sortActions, dataRects, finishedPlayback);
-}
-
-
-
-function generateElementRectangles(snap, array) {
-  var rects = [];
-  for (var i = 0; i < array.length; i++) {
-    var x = 5 + i * 12; // 10 + 2 padding
-    var width = 10;
-    var height = array[i] * 6;
-    var y = 125 - height;
-    var rect = snap.rect(x, y, width, height);
-    rects.push(rect);
-  }
-  return rects
-}
-
-function sort(data, dataRects, algorithm, customCompare) {
-  var sortActions = [];
-  algorithm.attachCompareObserver(function (data, a, b) {
-    sortActions.push(new SortAction(a, b, SortAction.COMPARE));
+function init() {
+  var initialArray = generateRandomArray();
+  sorts.forEach(function (sort) {
+    sort.pane = new SortPane(sort, initialArray);
   });
-  algorithm.attachSwapObserver(function (array, a, b) {
-    sortActions.push(new SortAction(a, b, SortAction.SWAP));
-  });
-  algorithm(data, customCompare);
-  algorithm.detachCompareObserver();
-  algorithm.detachSwapObserver();
-  // Run sortActions over dataRects
-  return sortActions;
+  initEventListeners();
 }
 
-function playSortActions(sortActions, dataRects, cb) {
-  var SPEED = 20;
-  if (sortActions.length === 0) {
-    cb();
-    return;
-  }
-  var action = sortActions.shift();
-  if (action.isSwapAction()) {
-    // Animate x values
-    var temp = dataRects[action.a].getBBox().x;
-    dataRects[action.a].animate({
-      x: dataRects[action.b].getBBox().x
-    }, SPEED);
-    dataRects[action.b].animate({
-      x: temp
-    }, SPEED);
-    // Swap indexes
-    temp = dataRects[action.a];
-    dataRects[action.a] = dataRects[action.b];
-    dataRects[action.b] = temp;
-  }
-  if (action.isCompareAction()) {
-    dataRects[action.a].attr({
-      fill: "#e0544c"
-    });
-    dataRects[action.b].attr({
-      fill: "#e0544c"
-    });
-  }
-
-  setTimeout(function () {
-    if (action.isCompareAction()) {
-      dataRects[action.a].attr({
-        fill: "#1e1e38"
-      });
-      dataRects[action.b].attr({
-        fill: "#1e1e38"
-      });
-    }
-    playSortActions(sortActions, dataRects, cb);
-  }, SPEED * 2);
-}
-
-function reverseCompare(a, b) {
-  return b - a;
-}
+init();
